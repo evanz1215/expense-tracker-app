10b0c82ca2f588f0657c657a4bc72137b9627d5b
Evan <evanz1215@gmail.com>
2026-02-16 02:03:38 +0800

    feat: add user home page with bottom navigation and transaction addition functionality

---
+++ app/user/add-transaction/index.tsx
@@ -0,0 +1,13 @@
+import SafeAreaLayoutWrapper from "@/safe-area-layout-wrapper";
+import React from "react";
+import { Text } from "react-native";
+
+const AddTransaction = () => {
+  return (
+    <SafeAreaLayoutWrapper>
+      <Text>AddTransaction</Text>
+    </SafeAreaLayoutWrapper>
+  );
+};
+
+export default AddTransaction;
--- app/user/home/index.tsx
+++ app/user/home/index.tsx
@@ -1,72 +1,72 @@
-import CustomButton from "@/components/custom-button";
-import CustomText from "@/components/custom-text";
 import { primaryColor } from "@/constants";
 import SafeAreaLayoutWrapper from "@/safe-area-layout-wrapper";
-import { logoutUser } from "@/services/users";
-import { useAuthStore } from "@/store/auth-store";
-import { useRouter } from "expo-router";
-import React from "react";
-import { View } from "react-native";
-import Toast from "react-native-toast-message";
+import { RelativePathString, useRouter } from "expo-router";
+import React, { useState } from "react";
+import { TouchableOpacity, View } from "react-native";
+import { BottomNavigation, Icon } from "react-native-paper";
+import HomeTab from "./_components/home-tab-";
+import ProfileTab from "./_components/profile-tab";
 
 const UserHomePage = () => {
-  const [loading, setLoading] = React.useState(false);
-  const { user, setUser } = useAuthStore();
+  const [index, setIndex] = useState(0);
   const router = useRouter();
 
-  const logoutHandler = async () => {
-    setLoading(true);
-    const response = await logoutUser();
+  const tabs = [
+    {
+      key: "home",
+      title: "Home",
+      component: HomeTab,
+      focusedIcon: "home",
+      unfocusedIcon: "home-outline",
+    },
+    {
+      key: "profile",
+      title: "Profile",
+      component: ProfileTab,
+      focusedIcon: "account",
+      unfocusedIcon: "account-outline",
+    },
+  ];
 
-    if (response.success) {
-      Toast.show({
-        type: "success",
-        text1: "Logout Successful",
-        text2: response.message,
-      });
-      setUser(null);
-      setLoading(false);
-      setTimeout(() => {
-        router.replace("/landing");
-      }, 500);
-    } else {
-      setLoading(false);
-      Toast.show({
-        type: "error",
-        text1: "Logout Failed",
-        text2: "An error occurred while logging out.",
-      });
-    }
-  };
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeTab,
+    profile: ProfileTab,
+  });
 
   return (
     <SafeAreaLayoutWrapper>
       <View
         style={{
-          flex: 1,
+          position: "absolute",
+          bottom: 90,
+          backgroundColor: primaryColor,
+          height: 60,
+          width: 60,
+          left: "50%",
+          zIndex: 1,
+          borderRadius: 30,
           justifyContent: "center",
           alignItems: "center",
-          gap: 20,
-          padding: 10,
+          transform: [{ translateX: -30 }],
         }}
       >
-        <CustomText
-          value={`Welcome ${user?.name || "User"}!`}
-          fontSize={30}
-          fontWeight="700"
-          color={primaryColor}
-        />
-
-        <CustomText
-          value={`Email: ${user?.email || "Not provided"}`}
-          fontSize={16}
-          fontWeight="600"
-        />
-
-        <CustomButton loading={loading} onPress={logoutHandler}>
-          Logout
-        </CustomButton>
+        <TouchableOpacity
+          onPress={() => {
+            router.push("/user/add-transaction" as RelativePathString);
+          }}
+        >
+          <Icon source="plus" size={30} color="#fff" />
+        </TouchableOpacity>
       </View>
+
+      <BottomNavigation
+        navigationState={{ index, routes: tabs }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+        barStyle={{ backgroundColor: "#e8e8e8ff", height: 80 }}
+        // add animation to tab transition
+        shifting={true}
+      />
     </SafeAreaLayoutWrapper>
   );
 };
