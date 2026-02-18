0b776c4b7e0bd87ebf5d1d30bd49245a61c821d7
Evan <evanz1215@gmail.com>
2026-02-19 02:30:04 +0800

    fix: correct IUser id type from string to number and add ITransaction interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,8 +1,19 @@
 export interface IUser {
-  id: string;
+  id: number;
   name: string;
   email: string;
   password?: string;
   profile_picture?: string;
+  create_at: string;
+}
+
+export interface ITransaction {
+  id: number;
+  user_id: number;
+  amount: number;
+  type: "income" | "expense";
+  category: string;
+  date: string;
+  description?: string;
   create_at: string;
 }
